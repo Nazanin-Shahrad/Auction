a8bf563d63be42a31e21eee6d99d50dcb05ee80d
nazaninshahrad <nazanin.shahrad@gmail.com>
2021-11-12 16:40:04 -0500

    add project for uploading to github

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,3 +2,4 @@
 require_once (__DIR__ . '/Config/config.php');
 require_once (__DIR__ . '/Lib/Functions.php');
 require_once (__DIR__ . "/../vendor/autoload.php");
+$session = new App\Lib\Session();
